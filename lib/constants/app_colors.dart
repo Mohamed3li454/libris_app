29beb4c22225ad325338243841b562315054b687
Mohamed3li454 <mohhamed4413@gmail.com>
2026-07-28 19:54:07 +0300

    feat: update project configuration and implement HomeView structure

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color background = Color(0xFFF0EADE);
+  static const Color primary = Color(0xFF8069BF);
+  static const Color secondary = Color(0xFF625D52);
+  static const Color accent = Color(0xFFC9A74D);
+  static const Color muted = Color(0xFF79767D);
+}
