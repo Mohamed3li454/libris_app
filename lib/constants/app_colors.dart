ab932adbeebdeb2cdd3b2ed059ac4dad05bcfdd7
Mohamed3li454 <mohhamed4413@gmail.com>
2026-07-29 16:02:49 +0300

    feat: add google_fonts, update primary color, and implement home view layout

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -4,7 +4,7 @@ class AppColors {
   AppColors._();
 
   static const Color background = Color(0xFFF0EADE);
-  static const Color primary = Color(0xFF8069BF);
+  static const Color primary = Color(0xFF765A1F);
   static const Color secondary = Color(0xFF625D52);
   static const Color accent = Color(0xFFC9A74D);
   static const Color muted = Color(0xFF79767D);
--- lib/features/home/presentation/view/widgets/home_view_body.dart
+++ lib/features/home/presentation/view/widgets/home_view_body.dart
@@ -1,14 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:libris_app/constants/app_colors.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        // Add your widgets here
-      ],
+    return SafeArea(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 28),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Libris",
+                  style: GoogleFonts.inter(
+                    color: AppColors.primary,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 32,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.search, size: 32, color: AppColors.primary),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
