e8df2660ea5a1312381eea3e4d278818bd929916
Mohamed3li454 <mohhamed4413@gmail.com>
2026-08-05 12:23:56 +0300

    feat: implement environment variable support using flutter_dotenv

---
+++ lib/constants/api_constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class ApiConstants {
+  ApiConstants._();
+
+  static String get apiKey => dotenv.env['API_KEY'] ?? '';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:libris_app/core/widgets/router.dart';
 import 'constants/app_colors.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
